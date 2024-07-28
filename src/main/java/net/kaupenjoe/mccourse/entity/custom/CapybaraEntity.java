945632a4758350dbf3345b7a6b407e037284efa2
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-07-28 16:29:40 +0200

    entity sounds

--- src/main/java/net/kaupenjoe/mccourse/entity/custom/CapybaraEntity.java
+++ src/main/java/net/kaupenjoe/mccourse/entity/custom/CapybaraEntity.java
@@ -9,7 +9,10 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -122,5 +125,25 @@ public class CapybaraEntity extends Animal {
         CapybaraVariant variant = Util.getRandom(CapybaraVariant.values(), this.random);
         this.setVariant(variant);
         return super.finalizeSpawn(pLevel, pDifficulty, pSpawnType, pSpawnGroupData);
+    }
+
+    /* SOUNDS */
+
+    @Nullable
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.PANDA_AMBIENT;
+    }
+
+    @Nullable
+    @Override
+    protected SoundEvent getHurtSound(DamageSource pDamageSource) {
+        return SoundEvents.PANDA_SNEEZE;
+    }
+
+    @Nullable
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.DONKEY_DEATH;
     }
 }
