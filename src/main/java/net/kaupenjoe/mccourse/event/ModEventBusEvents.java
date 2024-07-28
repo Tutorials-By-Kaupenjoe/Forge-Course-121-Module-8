dc3bff00337b6f7b8e6651e67dc691c19fef6ac7
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-07-28 16:24:57 +0200

    entity spawning

--- src/main/java/net/kaupenjoe/mccourse/event/ModEventBusEvents.java
+++ src/main/java/net/kaupenjoe/mccourse/event/ModEventBusEvents.java
@@ -5,8 +5,12 @@ import net.kaupenjoe.mccourse.entity.ModEntities;
 import net.kaupenjoe.mccourse.entity.client.CapybaraModel;
 import net.kaupenjoe.mccourse.entity.client.ModModelLayers;
 import net.kaupenjoe.mccourse.entity.custom.CapybaraEntity;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,5 +24,11 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.CAPYBARA.get(), CapybaraEntity.createAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void registerSpawnPlacement(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.CAPYBARA.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Animal::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 }
